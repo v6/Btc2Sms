14eea984419086878897cc2f3c3bf5862bc02327
Nathan Basanese <nathan@basanese.com>
2014-06-10 20:02:07 -0700

    This commits all the stuff that I have done with the Btc2Sms application.
    Things I have changed :
    
    0. I added the proguard.conf file to both the mobile directory and the root directory of this repository, basically, anywhere there is a pom.xml.
    1. I also added the
    
    For future builders of this repo:
    
    I added some instructions for how to build this without adding maven parameters, and some SERIOUS WARNINGS about keystore security.
    
    Check out (and checkout?)the README.md for this.

--- mobile/build_for_nathan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/home/nbas/bin/apache-maven-3.2.1/bin/mvn -e clean install -Dandroid.sdk.path=/home/nbas/bin/37coins/adt-bundle-linux-x86_64-20140321/sdk -P release -X -rf :btc2sms > random_degubbing_stuph.txt.log.nathan
